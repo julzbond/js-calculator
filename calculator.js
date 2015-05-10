4d02c33ad05034907890834d4a50621414b31eb9
Julia Bond <juliabond@me.com>
2015-05-09 15:24:48 -1000

    Final_Iteration

--- calculator.js
+++ calculator.js
@@ -6,7 +6,31 @@
  * @return {object} `calculator` object that can be used
  */
 
+var calculatorModule = function(){
 
+  var memory = 0;
+  var total = 0;
+  var calculator = {
+    load: validate(_load),
+    getTotal: _getTotal,
+    add: validate(_add),
+    subtract: validate(_subtract),
+    multiply: validate(_multiply),
+    divide: validate(_divide),
+    recall: _recall,
+    save: _save,
+    clear: _clear
+  };
+  return calculator;
+
+  function validate(fn){
+    return function(x){
+      if (typeof x !== "number"){
+        return "error";
+      }
+      return fn(x);
+    }
+  }
 
   /**
    * sets the `total` to the number passed in
@@ -14,11 +38,20 @@
    * @return { Number }    current total
    */
 
+   function _load(x){
+    total = x;
+    return x;
+    }
+
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+
+   function _getTotal(){
+    return total;
+   }
 
 
   /**
@@ -26,11 +59,18 @@
    * @param { Number } x
    */
 
+   function _add(x){
+    return total += x;
+   }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+
+   function _subtract(x){
+    return total -= x;
+   }
 
 
   /**
@@ -38,16 +78,28 @@
    * @param  { Number } x
    */
 
+   function _multiply(x){
+    return total *= x;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   function _divide(x){
+    return total /= x;
+   }
+
 
   /**
    * Stores the value of `total` to `memory`
    */
+
+   function _recall(total){
+    total = memory;
+    return memory;
+   }
 
 
   /**
@@ -55,8 +107,19 @@
    * @return { Number }
    */
 
+   function _save(){
+    memory = total;
+    return total;
+   }
+
 
   /**
    * Clear the value stored at `memory`
    */
 
+   function _clear(){
+    memory = 0;
+    return memory;
+   }
+
+}
